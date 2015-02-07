aca316c234c4a0f6af2e90fbcaa46450e3f7ad3d
jonathan <jonathan@kyuss.org>
2015-02-07 03:39:00 -0800

    add piwik standalone app

---
+++ apps/piwik-standalone/pre_bundle.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+function die {
+  echo 1>&2 "${1}"
+  exit 1
+}
+
+THISDIR=$(cd $(dirname $0) && pwd)
+CS_DIR=$(cd ${THISDIR}/../../../cloudstead-server && pwd)
+CS_JAR=$(find ${CS_DIR}/target -maxdepth 1 -type f -name "cloudstead-server-*.jar")
+PW_APP_DIR=$(cd ${CS_DIR}/../cloudos/cloudos-apps/apps/piwik && pwd)
+PW_MANIFEST="${PW_APP_DIR}/cloudos-manifest.json"
+
+if [ ! -f ${PW_MANIFEST} ] ; then
+  die "No manifest found: ${PW_MANIFEST}"
+fi
+
+JSON_EDIT="java -cp ${CS_JAR} org.cobbzilla.util.json.main.JsonEditor"
+
+rsync -avzc --exclude="target" ${PW_APP_DIR}/* ${THISDIR}/
+rm -rf \
+  ${THISDIR}/files/httpful.phar \
+  ${THISDIR}/files/CloudosAuthentication \
+  ${THISDIR}/templates/*
+
+cat ${PW_APP_DIR}/templates/config.ini.php.erb | grep -v CloudosAuthentication > ${THISDIR}/templates/config.ini.php.erb
+
+cat ${PW_MANIFEST} \
+  | ${JSON_EDIT} -o delete -p assets \
+  | ${JSON_EDIT} -o delete -p copy \
+  | ${JSON_EDIT} -o delete -p auth \
+> ${THISDIR}/cloudos-manifest.json || die "Error editing json"
