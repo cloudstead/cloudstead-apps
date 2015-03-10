43378c4c52161529772fcabedc54059ea08dd3a0
jonathan <jonathan@kyuss.org>
2015-03-10 08:53:16 -0700

    support db migration

--- apps/piwik-standalone/pre_bundle.sh
+++ apps/piwik-standalone/pre_bundle.sh
@@ -29,8 +29,8 @@ cat ${PW_MANIFEST} \
   | ${JSON_EDIT} -o delete -p assets \
   | ${JSON_EDIT} -o delete -p copy \
   | ${JSON_EDIT} -o delete -p auth \
-  | ${JSON_EDIT} -o write -p databags -v \
+  | ${JSON_EDIT} -o write -p config -v \
     "[ { \"name\": \"init\", \"items\": [ \"admin.login\", \"admin.password\", \"admin.email\" ] } ]" \
   | ${JSON_EDIT} -o write -p post_install -v \
-    "[ { \"exec\": \"@files/piwik_user_add.sh @dbname @databag[init][admin][login] @databag[init][admin][password] @databag[init][admin][email] 1\" } ]" \
+    "[ { \"exec\": \"@files/piwik_user_add.sh @dbname @config[init][admin][login] @config[init][admin][password] @config[init][admin][email] 1\" } ]" \
 > ${THISDIR}/cloudos-manifest.json || die "Error editing json"
